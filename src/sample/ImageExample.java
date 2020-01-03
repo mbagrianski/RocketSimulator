26bb6ed2f479482e9eb52c87aef158f124fa3def
mbagrianski <46272573+mbagrianski@users.noreply.github.com>
2020-01-03 18:08:24 -0500

    Deleted application/Rocket.java- unnecessary

--- src/sample/ImageExample.java
+++ src/sample/ImageExample.java
@@ -1,14 +1,8 @@
 package sample;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-import application.Rocket;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class ImageExample extends Application {
